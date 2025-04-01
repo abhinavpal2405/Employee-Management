89d6de4138b713b22eaad8916158169babf9ecf7
ABHINAV PAL <114683444+abhinavpal2405@users.noreply.github.com>
2025-04-01 22:48:03 +0530

    Delete server directory

--- server/routes/auth.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-import { login } from '../controllers/authController.js'
-const loginRouter= express.Router()
-loginRouter.post('/login',login)
-export default loginRouter;
--- server/routes/register.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-import {register} from '../controllers/registerController.js'
-const registerRouter= express.Router()
-registerRouter.post('/register',register)
-export default registerRouter;
-
