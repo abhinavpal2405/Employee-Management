646ae9000c9ec7683cddfa8b5ee23eebbfb16853
ANUJ KUMAR <98866899+anujjsengar@users.noreply.github.com>
2025-04-26 00:17:07 +0530

    Update EmployeeData.jsx

--- frontend/src/InfoRetrival/EmployeeData.jsx
+++ frontend/src/InfoRetrival/EmployeeData.jsx
@@ -11,7 +11,7 @@ const EmployeeData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/get-details/employees");
+        const response = await axios.get("https://employee-management-eyf8.onrender.com/api/get-details/employees");
         setEmployees(response.data);
         setFilteredEmployees(response.data);
       } catch (error) {
